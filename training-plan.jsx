9e554f9a8a01adc517abf22f53145020f4b3c3f2
Clément Savalle-Anthonioz <savalle.clement@gmail.com>
2026-06-29 23:20:49 +0100

    chore: verify fixed Babel build

--- training-plan.jsx
+++ training-plan.jsx
@@ -1,4 +1,4 @@
-// @version 1782771516
+// @version 1782771649
 import { useState, useEffect } from "react";
 
 const GH_TOKEN = "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";
