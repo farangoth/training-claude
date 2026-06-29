79e159b2fb094f3719d64e301e40d90e902a2034
Clément Savalle-Anthonioz <savalle.clement@gmail.com>
2026-06-29 23:25:08 +0100

    chore: trigger inlined build workflow

--- training-plan.jsx
+++ training-plan.jsx
@@ -1,4 +1,4 @@
-// @version 1782771773
+// @version 1782771908
 import { useState, useEffect } from "react";
 
 const GH_TOKEN = "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";
